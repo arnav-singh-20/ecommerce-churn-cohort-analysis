67d8d1db0d91b8fc1574722cc8ea83acb2a212ee
Arnav Singh <arnavwork1062@gmail.com>
2026-04-25 10:54:46 +0530

    Update core_metrics.sql

--- sql/core_metrics.sql
+++ sql/core_metrics.sql
@@ -1,38 +1,31 @@
--- ==========================================
--- 01_data_cleaning.sql
--- Purpose:
--- 1. Rename raw imported columns
--- 2. Assign business meaning
--- 3. Clean invalid and cancelled transactions
--- ==========================================
-
--- Step 1: Create semantic raw_orders table
-CREATE TABLE raw_orders AS
+SELECT 
+  ROUND(SUM(order_value), 2) AS total_revenue
+FROM orders;
+SELECT 
+  COUNT(DISTINCT invoice_no) AS total_orders
+FROM orders;
+SELECT 
+  ROUND(
+    SUM(order_value) * 1.0 / COUNT(DISTINCT invoice_no),
+    2
+  ) AS AOV
+FROM orders;
 SELECT
-  field1 AS invoice_no,
-  field2 AS stock_code,
-  field3 AS description,
-  CAST(field4 AS INTEGER) AS quantity,
-  field5 AS invoice_date,
-  CAST(field6 AS REAL) AS unit_price,
-  CAST(field7 AS INTEGER) AS customer_id,
-  field8 AS country
-FROM data;
-
--- Step 2: Create clean orders table using business rules
-CREATE TABLE orders AS
+  ROUND(
+    SUM(order_value) * 1.0 / COUNT(DISTINCT customer_id),
+    2
+  ) AS ARPU
+FROM orders;
 SELECT
-  invoice_no,
-  stock_code,
-  description,
-  quantity,
-  invoice_date,
-  unit_price,
-  customer_id,
   country,
-  quantity * unit_price AS order_value
-FROM raw_orders
-WHERE invoice_no NOT LIKE 'C%'        -- remove cancelled orders
-  AND quantity > 0                    -- remove invalid quantity
-  AND unit_price > 0                  -- remove invalid price
-  AND customer_id IS NOT NULL;         -- ensure user-level analysis
+  ROUND(SUM(order_value), 2) AS revenue
+FROM orders
+GROUP BY country
+ORDER BY revenue DESC
+LIMIT 10;
+SELECT
+  SUBSTR(invoice_date, 1, 7) AS year_month,
+  ROUND(SUM(order_value), 2) AS monthly_revenue
+FROM orders
+GROUP BY year_month
+ORDER BY year_month;
